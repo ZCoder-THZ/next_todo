dd9175cc206cf676be55870026773e80d513cf43
Than Htike Zaw <thanhtikezaw919@gmail.com>
2023-12-17 10:10:44 +0630

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,115 @@
-import Image from 'next/image'
+'use client';
 
-export default function Home() {
+import React,{useState,useRef} from 'react'
+// import Modal from '@/components/modals/Modal';
+
+
+
+const page = () => {
+  const data=[
+    {id:1,task:'mowiing the lawn',is_done:false},
+    {id:2,task:'learn lesson',is_done:false},
+    {id:3,task:'learn programming',is_done:true}
+
+  ]
+  const [todos,setTodos]=useState(data);
+  const [error,setError]=useState(false);
+  const [modal,setModal]=useState(false)
+  const task=useRef();
+  const finishTask=(id)=>{
+    const taskId=todos.findIndex(todo=>todo.id==id);
+  
+    if(taskId!==-1){
+      const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, is_done: !todo.is_done } : todo
+
+    );
+      setTodos(updatedTodos)
+    }
+  }
+  const removeTodo=id=>{
+    const taskId=todos.findIndex(todo=>todo.id==id);
+      
+    if(taskId!==-1){
+     
+      const removeTask=todos.filter(todo=>todo.id!==id);
+      
+
+      setTodos(removeTask)
+    }
+
+  }
+  const addTask=(event)=>{
+    const value=task.current.value;
+    if(value!==''){
+      setTodos([...todos,{id:todos.length+1,task:value,is_done:false}])
+      cleanTask()
+      setError(false)
+
+    }else{
+      setError(true)
+    }
+  
+
+
+  }
+  const handleKeyPress=()=>{
+    if (event.key === 'Enter') {
+      // Call your function to handle the Enter key press here
+      addTask()
+
+    }
+  }
+  const cleanTask=()=>{
+    task.current.value=''
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    
+<div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+	<div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div className="mb-4">
+            <h1 className="text-grey-darkest">Todo List</h1>
+            <div className="flex mt-4">
+                <input onKeyDown={handleKeyPress} ref={task} className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo"/>
+                <button onClick={addTask} className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">Add</button>
+            </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {
+          error?   <div role="alert mb-4">
+          <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+            Danger
+          </div>
+          <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+            <p>Something not ideal might be happening.</p>
+          </div>
+            </div>:''
+        }
+     
+        <div>
+            {
+              todos.map((todo)=>(
+                <div key={todo.id} className="flex mb-4 items-center" >
+                <p className={todo.is_done ? 'line-through w-full text-green' : 'w-full'}>{todo.task}</p>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green" onClick={()=>{
+                  finishTask(todo.id)
+                }}>Done</button>
+                <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red" onClick={()=>{
+                  removeTodo(todo.id)
+                }}>Remove</button>
+            </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              ))
+            }
+         
+        </div>
+            {/* {
+              modal?<Modal/>:''
+            } */}
+    </div>
+</div>
   )
 }
+
+export default page;
